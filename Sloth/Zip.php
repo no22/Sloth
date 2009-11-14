417060dbda670e197e4666bc0d4d36ff661a186b
no22 <Hiroyuki.no22@gmail.com>
2009-11-15 08:07:06 +0900

    5.2対応

--- Sloth/Zip.php
+++ Sloth/Zip.php
@@ -26,40 +26,87 @@
  *|30
  * <<<<
  */
-class Sloth_Zip extends Sloth_Iterator
-{
-    protected $callback = null;
 
-    public function __construct($seq, $aSeq, $callback = null)
+if (version_compare(PHP_VERSION, '5.3', '>=')) {
+    class Sloth_Zip extends Sloth_Iterator
     {
-        $iterator = Sloth::iter($seq);
-        $itMulti = new MultipleIterator(
-            MultipleIterator::MIT_NEED_ALL | MultipleIterator::MIT_KEYS_NUMERIC
-        );
-        $itMulti->attachIterator($iterator);
-        foreach ($aSeq as $mSeq) {
-            $itMulti->attachIterator(Sloth::iter($mSeq));
+        protected $callback = null;
+
+        public function __construct($seq, $aSeq, $callback = null)
+        {
+            $iterator = Sloth::iter($seq);
+            $itMulti = new MultipleIterator(
+                MultipleIterator::MIT_NEED_ALL | MultipleIterator::MIT_KEYS_NUMERIC
+            );
+            $itMulti->attachIterator($iterator);
+            foreach ($aSeq as $mSeq) {
+                $itMulti->attachIterator(Sloth::iter($mSeq));
+            }
+            $this->iterator = $itMulti;
+            if (!is_null($callback)) {
+                if (!is_callable($callback)) throw new InvalidArgumentException;
+                $this->callback = $callback;
+            }
         }
-        $this->iterator = $itMulti;
-        if (!is_null($callback)) {
+
+        public function __clone()
+        {
+            parent::__clone();
+            $this->callback = clone $this->callback;
+        }
+
+        public function current()
+        {
+            $callback = $this->callback;
+            if (is_null($callback)) {
+                return $this->iterator->current();
+            }
+            return call_user_func_array($callback, $this->iterator->current());
+        }
+    }
+}
+else {
+    class Sloth_Zip extends Sloth_Iterator
+    {
+        protected $callback = null;
+        protected $iterator2 = null;
+        
+        function __construct($seq, $aSeq, $callback = null)
+        {
+            parent::__construct($seq);
+            $this->iterator2 = Sloth::iter($aSeq[0]);
             if (!is_callable($callback)) throw new InvalidArgumentException;
             $this->callback = $callback;
         }
-    }
 
-    public function __clone()
-    {
-        parent::__clone();
-        $this->callback = clone $this->callback;
-    }
-
-    public function current()
-    {
-        $callback = $this->callback;
-        if (is_null($callback)) {
-            return $this->iterator->current();
+        public function __clone()
+        {
+            parent::__clone();
+            $this->iterator2 = clone $this->iterator2;
         }
-        return call_user_func_array($callback, $this->iterator->current());
+
+        public function current()
+        {
+            return call_user_func($this->callback, $this->iterator->current(), $this->iterator2->current());
+        }
+        
+        public function next()
+        {
+            $this->iterator->next();
+            $this->iterator2->next();
+        }
+        
+        public function valid()
+        {
+            return $this->iterator->valid() && $this->iterator2->valid();
+        }
+        
+        public function rewind()
+        {
+            $this->iterator->rewind();
+            $this->iterator2->rewind();
+        }
+
     }
 }
 
